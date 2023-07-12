56afd6bc5fb67e0c022ad9a9eb76fd845e1e34e4
ddr <138032878+fdjhdfucjqbsdh@users.noreply.github.com>
2023-07-12 18:20:04 +0700

    Update On-Demand.js

--- asset/On-Demand.js
+++ asset/On-Demand.js
@@ -5,7 +5,7 @@ for (var i = 0; i < videos.length; i++) { // for each
         var iframe = document.createElement('iframe'); // create <iframe>
         iframe.setAttribute('allowfullscreen', ''); // set attributes
         iframe.setAttribute('allow', 'autoplay; fullscreen');
-        iframe.setAttribute('src', 'https://likessb.com/e/'+ this.dataset.videoid +'?autoplay=1'); // populate video ID
+        iframe.setAttribute('src', 'https://likessb.com/e/{{ $id }}'); // populate video ID
         this.innerHTML = ''; // remove existing HTML (<a> & <img>)
         this.appendChild(iframe); // replace with <iframe>
         event.preventDefault(); // prevent link action
